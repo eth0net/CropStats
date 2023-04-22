2a9ef2e1949bf93945405bba89256cebabef9aa4
Elliot Thomas <elliot@eth0net.io>
2023-04-22 15:15:54 +0100

    Implement crop stats comp

---
+++ Source/CropStats.cs
@@ -0,0 +1,17 @@
+﻿using Verse;
+
+namespace CropStats;
+
+/// <summary>
+/// CropStats class to load up the mod and initialise everything.
+/// </summary>
+public class CropStats : Mod
+{
+    /// <summary>
+    /// CropStats constructor to load the mod and settings.
+    /// </summary>
+    /// <param name="content"></param>
+    public CropStats(ModContentPack content) : base(content)
+    {
+    }
+}
